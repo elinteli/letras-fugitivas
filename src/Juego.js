b1181bd814b48b85d3862d438355c85dbb5532ff
elinteli <creaciones.inteli@gmail.com>
2024-08-18 17:22:42 -0300

    #

--- src/Juego.js
+++ src/Juego.js
@@ -42,10 +42,10 @@ export default function Juego({ alerta }) {
       document.querySelector("#body").className = 'efectoPantallaColor';
       document.querySelector(':root').style.setProperty('--color-efectopantalla', 'var(--efecto-pantalla-negro)');
 
-      document.querySelector('.config__input--volumen').checked = JSON.parse(localStorage.getItem("sonidoLF"));
+      document.querySelector('.config__input--volumen').checked = volumenSnd;
       document.querySelector('.config__input--volumen').addEventListener('change', function () {
-         localStorage.setItem("sonidoLF", volumenSnd);
          volumenSnd = document.querySelector('.config__input--volumen').checked;
+         localStorage.setItem("sonidoLF", volumenSnd);
       });
 
       document.querySelector('.config__btn--reiniciar').addEventListener('click', function () {
